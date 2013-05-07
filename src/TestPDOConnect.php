85db32a90af9e85468f05fa21fad90d2cbfc38c7
Irwan Krisna <irwan.krisna@anu.edu.au>
2013-05-07 16:52:03 +1000

    comment

--- src/TestPDOConnect.php
+++
@@ -1,7 +0,0 @@
-<?php
-$dbh = new PDO("mysql:host=localhost;dbname=oaidb", "irwan", "");
-$sth = $dbh->query ("SELECT serial, dc_title FROM oai_records where serial = 1");
-while ($row = $sth->fetch (PDO::FETCH_ASSOC)) {
-printf ("Name: %s, Category: %s\n", $row["serial"], $row["dc_title"]);
-}
-?>
